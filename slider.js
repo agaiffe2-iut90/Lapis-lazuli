3e4b025c0fdbe545c27169dca1d4a228f7d3468a
sdziggar-iut90 <146032137+sdziggar-iut90@users.noreply.github.com>
2023-10-22 19:19:15 +0200

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,11 @@
+const slide = ["im2.jpg", "im3.jpg", "im4.jpg", "im5.jpg","im6.jpg"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
